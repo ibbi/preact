f7bd8985fa1ff5939fa735430b4ce94825e6528f
ibbi <hi@ibbi.io>
2019-04-29 21:05:45 -0700

    todos

--- one/src/App.js
+++ one/src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import Item from './Item';
 
 function App() {
+	const toDos = [
+		'duolingo',
+		'react daily',
+		'study or build',
+		'hit macros',
+		'reach out',
+		'train',
+	];
 	return (
 		<div className='App'>
-			<Item>mess</Item>
+			{toDos.map((i, j) => (
+				<Item key={j}>{i}</Item>
+			))}
 		</div>
 	);
 }
