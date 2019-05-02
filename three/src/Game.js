06ce38f991e40c7132fb973afd3abd9da7d0fbce
ibbi <hi@ibbi.io>
2019-05-01 20:24:28 -0700

    madew rong game lol

--- three/src/Game.js
+++ three/src/Game.js
@@ -8,25 +8,34 @@ export class Game extends Component {
 		enteredSequence: [],
 		gameOver: false,
 	};
+    colors = ['green', 'yellow', 'blue', 'red'];
+    
 	componentDidMount() {
 		let rand = this.colors[Math.floor(Math.random() * this.colors.length)];
 		this.setState({ sequence: [...this.state.sequence, rand] });
-	}
-	componentDidUpdate() {
-        
-    }
-	colors = ['green', 'yellow', 'blue', 'red'];
+    };
+    
+	arrayEquality = (a1, a2) => {
+		return JSON.stringify(a1) == JSON.stringify(a2);
+    };
+    
 	pressed = (color) => {
-		if (color === this.state.sequence[this.state.sequence.length - 1]) {
-			let rand = this.colors[Math.floor(Math.random() * this.colors.length)];
-			this.setState({
-				enteredSequence: [...this.state.enteredSequence, color],
-				sequence: [...this.state.sequence, rand],
-			});
-		} else {
-			this.setState({ gameOver: true });
-		}
+        this.setState({
+            enteredSequence: [...this.state.enteredSequence, color],
+        });
+		if (this.state.sequence.length === this.state.enteredSequence.length) {
+			if (this.arrayEquality(this.state.enteredSequence, this.state.sequence)) {
+				let rand = this.colors[Math.floor(Math.random() * this.colors.length)];
+				this.setState({
+					enteredSequence: [],
+					sequence: [...this.state.sequence, rand],
+				});
+			} else {
+				this.setState({ gameOver: true });
+			}
+		} 
 	};
+
 	render() {
 		return this.state.gameOver ? (
 			<h1>Game Over, score: {this.state.enteredSequence.length}</h1>
