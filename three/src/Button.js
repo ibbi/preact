c72982163c07005f6c73b6cd9d35384c3f527926
ibbi <hi@ibbi.io>
2019-05-01 23:05:59 -0700

    getelemtn eugh i tried

--- three/src/Button.js
+++ three/src/Button.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Button = (props) => {
-	return <div onClick={props.handler} className={props.className} />;
+	return (
+		<div onClick={props.handler} id={props.id} className={props.className} />
+	);
 };
 
 export default Button;
--- three/src/Game.js
+++ three/src/Game.js
@@ -8,12 +8,14 @@ export class Game extends Component {
 		sequence: [],
 		enteredSequence: [],
 		gameOver: false,
+		bordered: [0, 0, 0, 0],
 	};
 
 	componentDidMount() {
 		let rand = colors[Math.floor(Math.random() * colors.length)];
 		this.setState({ sequence: [...this.state.sequence, rand] });
 	}
+
 	componentDidUpdate() {
 		console.log(this.state.sequence);
 	}
@@ -26,10 +28,17 @@ export class Game extends Component {
 			() => {
 				if (Equality(this.state.enteredSequence, this.state.sequence)) {
 					let rand = colors[Math.floor(Math.random() * colors.length)];
-					this.setState({
-						enteredSequence: [],
-						sequence: [...this.state.sequence, rand],
-					});
+					this.setState(
+						{
+							enteredSequence: [],
+							sequence: [...this.state.sequence, rand],
+						},
+						() => {
+							this.state.sequence.forEach((i, j) => {
+								document.getElementById(i).classList.add('bordered');
+							});
+						}
+					);
 				} else if (
 					this.state.enteredSequence.length === this.state.sequence.length
 				) {
@@ -41,15 +50,20 @@ export class Game extends Component {
 
 	render() {
 		return this.state.gameOver ? (
-			<h1>Game Over, score: {this.state.enteredSequence.length}</h1>
+			<h1>Game Over, score: {this.state.sequence.length - 1}</h1>
 		) : (
 			<>
 				<div className='buttonCont'>
 					{colors.map((i, j) => (
-						<Button key={j} handler={() => this.pressed(i)} className={i} />
+						<Button
+							key={j}
+							handler={() => this.pressed(i)}
+							className='box'
+							id={i}
+						/>
 					))}
 				</div>
-				<Score display={this.state.enteredSequence.length} />
+				<Score display={this.state.sequence.length - 1} />
 			</>
 		);
 	}
