c89635b18b7dae2d4638eb4a326393fce27ed968
ibbi <hi@ibbi.io>
2019-05-04 20:08:49 -0700

    wow

--- five/src/App.js
+++ five/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import Post from './Post';
+import { Posts } from './Data';
 
 function App() {
 	return (
 		<div className='mainCont'>
-			<Header />;<Post />
+			<Header />
+			{Posts.map((i) => (
+				<Post
+					title={i.title}
+					points={i.points}
+					poster={i.poster}
+					time={i.time}
+					comments={i.comments}
+				/>
+			))}
 		</div>
 	);
 }
--- five/src/Post.js
+++ five/src/Post.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { titles } from './Data';
 
-const Post = () => {
+const Post = ({ title, points, poster, time, comments }) => {
 	return (
 		<div className='postCont'>
-			{titles.map((i) => (
-				<h2>{i}</h2>
-			))}
+			<h2>{title}</h2>
+			<div className='buttonsCont'>
+				<p>
+					{points} points by {poster} {time} hour ago | {comments} comments
+				</p>
+			</div>
 		</div>
 	);
 };
 
 export default Post;
+
+//14 points by clouddrover 1 hour ago | hide | 6 comments
